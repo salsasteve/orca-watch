16270e73b9d249cd7f9cd2f673ab684032834d13
Salsa Steve <salsasteve@metacanny.com>
2022-04-17 15:34:54 -0500

    change config to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   ssr: true,
-  target: 'server',
+  target: 'static',
   head: {
     title: 'orca-watch',
     htmlAttrs: {
