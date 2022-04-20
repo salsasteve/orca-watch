7cacc3491241e42ffb2b2dfb50e83930a1568b82
Salsa Steve <salsasteve@metacanny.com>
2022-04-20 14:57:09 -0500

    Adding table details

---
+++ components/table/Degen.vue
@@ -0,0 +1,83 @@
+<template>
+  <section>
+    <b-table
+      :data="isEmpty ? [] : data"
+      :bordered="isBordered"
+      :striped="isStriped"
+      :narrowed="isNarrowed"
+      :hoverable="isHoverable"
+      :loading="isLoading"
+      :focusable="isFocusable"
+      :mobile-cards="hasMobileCards"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      :pagination-position="paginationPosition"
+      :default-sort-direction="defaultSortDirection"
+      :pagination-rounded="isPaginationRounded"
+      :sort-icon="sortIcon"
+      :sort-icon-size="sortIconSize"
+      default-sort="added"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      :page-input="hasInput"
+    >
+      <b-table-column v-slot="props" field="name" label="Name">{{ props.row.name }}</b-table-column>
+      <b-table-column v-slot="props" field="symbol" label="Symbol">{{ props.row.symbol }}</b-table-column>
+      <b-table-column v-slot="props" field="added" label="Date Added"> {{ props.row.added }} </b-table-column>
+
+      <template #empty>
+        <div class="has-text-centered">No records</div>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+import EthplorerService from '../../services/EthplorerService'
+
+export default {
+  name: 'DegenTable',
+  data() {
+    return {
+      data: [],
+      isEmpty: false,
+      isBordered: false,
+      isStriped: false,
+      isNarrowed: false,
+      isHoverable: false,
+      isFocusable: false,
+      isLoading: false,
+      hasMobileCards: true,
+      isPaginated: true,
+      isPaginationSimple: false,
+      isPaginationRounded: true,
+      paginationPosition: 'bottom',
+      defaultSortDirection: 'asc',
+      sortIcon: 'arrow-up',
+      sortIconSize: 'is-small',
+      currentPage: 1,
+      perPage: 10,
+      hasInput: false,
+    }
+  },
+  async fetch() {
+    try {
+      this.loading = true
+      const { data } = await EthplorerService.getTokensNew()
+      this.data = data
+      console.log(this.data)
+      this.loading = false
+    } catch (e) {
+      this.data = []
+      this.loading = false
+      console.log(e)
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/degen/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-taglist>
+      <b-tag type="is-primary" size="is-large">Degen Dashboard</b-tag>
+    </b-taglist>
+    <DegenTable />
+  </div>
+</template>
+
+<script>
+import DegenTable from '../../components/table/Degen'
+
+export default {
+  components: {
+    DegenTable,
+  },
+  head() {
+    return {
+      title: this.$route.params.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for Blockchain Analytics',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
