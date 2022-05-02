a64cea526a2a1789200f9ab3e776e123d1428a32
Salsa Steve <salsasteve@metacanny.com>
2022-05-02 09:50:09 -0500

    Adding address checker

--- components/layout/AppNavBar.vue
+++ components/layout/AppNavBar.vue
@@ -4,6 +4,7 @@
       <b-navbar-item tag="nuxt-link" to="/">Home</b-navbar-item>
       <b-navbar-item tag="nuxt-link" to="/tokens">Tokens</b-navbar-item>
       <b-navbar-item tag="nuxt-link" to="/degen">Degen</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/address">Address</b-navbar-item>
     </template>
   </b-navbar>
 </template>
